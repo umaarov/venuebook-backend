e32ceaa15da95ea66bb683ba1ade9f02e5a6f4b3
umaarov <hs.umarov21@gmail.com>
2025-05-08 16:27:43 +0500

    feat: return application details as JSON response on root route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'version' => '1.0.0',
+        'documentation' => url('/swagger'),
+        'framework' => 'Laravel',
+        'framework_version' => app()->version(),
+        'php_version' => PHP_VERSION,
+        'environment' => app()->environment(),
+        'app_name' => config('app.name'),
+        'app_url' => config('app.url'),
+        'app_env' => config('app.env'),
+        'app_locale' => config('app.locale'),
+        'app_timezone' => config('app.timezone'),
+    ]);
 });
