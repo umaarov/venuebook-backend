e7c9397f648cda2e21044cf28dde6d8053094383
umaarov <hs.umarov21@gmail.com>
2025-05-19 11:17:46 +0500

    feat: enhance API response structure with additional metadata and organization + aws cicd test

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,34 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'version' => '1.0.0',
+        'api_version' => '1.0.0',
+        'contact' => [
+            'email' => 'hs.umarov21@gmail.com',
+        ],
         'documentation' => url('/swagger'),
+        'repo' => 'https://github.com/umaarov/venuebook-backend',
+
+        'version' => '1.0.0',
         'framework' => 'Laravel',
         'framework_version' => app()->version(),
         'php_version' => PHP_VERSION,
         'environment' => app()->environment(),
-        'app_name' => config('app.name'),
-        'app_url' => config('app.url'),
-        'app_env' => config('app.env'),
-        'app_locale' => config('app.locale'),
-        'app_timezone' => config('app.timezone'),
+
+        'app' => [
+            'name' => config('app.name'),
+            'url' => config('app.url'),
+            'env' => config('app.env'),
+            'locale' => config('app.locale'),
+            'timezone' => config('app.timezone'),
+        ],
+
+        'server' => [
+            'time' => now()->toIso8601String(),
+        ],
+
+        'security' => [
+            'authentication' => 'Bearer Token',
+        ],
     ]);
 });
+
